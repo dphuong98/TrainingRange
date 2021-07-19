27a40aa04435abfa348b0b7adeabe7fd420a4675
Doan Duy Phuong <dphuongmeo@gmail.com>
2021-07-19 16:56:07 +0700

    PriorityLinkedList: completed
    Test by using context menu "PriorityLinkedList test" in PriorityLinkedListTest component

---
+++ Assets/Scripts/Extension/Collections/LinkedListExtension.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class LinkedListExtension
+{
+    private static LinkedListNode<T> MiddleNode<T>(LinkedListNode<T> start, LinkedListNode<T> last)
+    {
+        if (start == null)
+            return null;
+
+        var slow = start;
+        var fast = start.Next;
+
+        while (fast != last)
+        {
+            fast = fast.Next;
+            if (fast != last)
+            {
+                slow = slow.Next;
+                fast = fast.Next;
+            }
+        }
+
+        return slow;
+    }
+    
+    public static LinkedListNode<T> BinarySearch<T>(this LinkedList<T> data, T item) where T : IComparable<T>
+    {
+        var start = data.First;
+        LinkedListNode<T> last = null;
+
+        do
+        {
+            var mid = MiddleNode(start, last);
+
+            if (mid == null)
+                return null;
+
+            if (mid.Value.CompareTo(item) == 0)
+                return mid;
+            
+            if (mid.Value.CompareTo(item) < 0)
+            {
+                start = mid.Next;
+            }
+            else
+                last = mid;
+            
+        } while (last == null || last != start);
+
+        return last;
+    }
+    
+    public static LinkedListNode<T> BinarySearch<T>(this LinkedList<T> data, T item, IComparer<T> comparer)
+    {
+        var start = data.First;
+        LinkedListNode<T> last = null;
+
+        do
+        {
+            var mid = MiddleNode(start, last);
+
+            if (mid == null)
+                return null;
+
+            if (comparer.Compare(mid.Value, item) == 0)
+                return mid;
+            
+            if (comparer.Compare(mid.Value, item) < 0)
+            {
+                start = mid.Next;
+            }
+            else
+                last = mid;
+            
+        } while (last == null || last != start);
+
+        return last;
+    }
+}
---
+++ Assets/Scripts/Test/PriorityLinkedListTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+public class PriorityLinkedListTest : MonoBehaviour
+{
+    private Random rand;
+
+    [ContextMenu("PriorityLinkedList test")]
+    private void Start()
+    {
+        rand = new Random();
+        
+        var integers = new PriorityLinkedList<int>();
+        for (var i = 0; i < 10; i++)
+        {
+            integers.Add(rand.Next(100));
+        }
+        Debug.Log(string.Join(", ", integers));
+        
+        var strings = new PriorityLinkedList<string>();
+        for (var i = 0; i < 10; i++)
+        {
+            strings.Add(String.Random(5));
+        }
+        Debug.Log(string.Join(", ", strings));
+    }
+}
