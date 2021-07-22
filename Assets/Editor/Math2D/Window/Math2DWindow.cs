3796097c7eded5f606a033e66e8066748bacb921
Doan Duy Phuong <dphuongmeo@gmail.com>
2021-07-22 11:31:46 +0700

    Visualizer: spawn point
    +Cartesian coordinate system
    +Point prefab

---
+++ Assets/Editor/Math2D/Window/Math2DWindow.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Linq;
+public class Math2DWindow : EditorWindow
+{
+    private float buttonWidth = 50f;
+    private float buttonHeight = 50f;
+
+    private GameObject math2D;
+    private GameObject pointPrefab;
+
+    private Transform points;
+
+    [MenuItem("Custom/Math2D")]
+    private static void ShowWindow()
+    {
+        GetWindow<Math2DWindow>("Math2D");
+    }
+
+    private void Awake()
+    {
+        math2D = GameObject.Find("Math2D");
+        if (math2D == null)
+        {
+            math2D = (GameObject) Instantiate(Resources.Load("Prefabs/Math2D/Math2D"));
+            math2D.name = "Math2D";
+        }
+
+        points = math2D.transform.Find("Points");
+        
+        pointPrefab = Resources.Load("Prefabs/Math2D/Point") as GameObject;
+        Debug.Log(pointPrefab);
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button(Resources.Load<Texture>("Icons/Point"), GUILayout.Width(buttonWidth),
+            GUILayout.Height(buttonHeight)))
+        {
+            SpawnPoint();
+        }
+        GUILayout.EndHorizontal();
+    }
+
+    private void SpawnPoint()
+    {
+        var cameraPosition = SceneView.lastActiveSceneView.camera.transform.position;
+        var newPoint = Instantiate(pointPrefab, cameraPosition, Quaternion.identity, points);
+        foreach (var c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        {
+            if (points.Find(c.ToString()) != null)
+                continue;
+
+            newPoint.name = c.ToString();
+            break;
+            //TODO naming method after "Z"
+        }
+
+        Selection.activeTransform = newPoint.transform;
+        SceneView.lastActiveSceneView.FrameSelected();
+    }
+}
---
+++ Assets/Scripts/4_Math2D/Gizmo/Axis.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+public class Axis : MonoBehaviour
+{
+    public enum AxisType
+    {
+        X,
+        Y,
+        Z
+    }
+
+    public Color AxisColor = Color.black;
+    public AxisType SelectedAxis;
+    public int Length;
+
+    private void OnDrawGizmos()
+    {
+        Vector3 start = new Vector3();
+        Vector3 end = new Vector3();
+        switch (SelectedAxis)
+        {
+            case AxisType.X:
+                start.x = Length;
+                end.x = -Length;
+                break;
+            case AxisType.Y:
+                start.y = Length;
+                end.y = -Length;
+                break;
+            case AxisType.Z:
+                start.z = Length;
+                end.z = -Length;
+                break;
+        }
+
+        var thickness = (Vector3)(Vector2.Perpendicular(start - end).normalized * 1/50);
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(start, end);
+        Gizmos.DrawLine(start + thickness, end + thickness);
+        Gizmos.DrawLine(start - thickness, end - thickness);
+    }
+}
