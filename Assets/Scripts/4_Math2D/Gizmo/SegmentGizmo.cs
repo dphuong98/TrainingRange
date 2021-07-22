f0009484d1b9b26f43dfc1a8ad9ce3b88baa75c3
Doan Duy Phuong <dphuongmeo@gmail.com>
2021-07-22 17:49:26 +0700

    draft: bugfix for segment that got one of their point destroyed

--- Assets/Scripts/4_Math2D/Gizmo/SegmentGizmo.cs
+++ Assets/Scripts/4_Math2D/Gizmo/SegmentGizmo.cs
@@ -8,9 +8,13 @@ public class SegmentGizmo : MonoBehaviour
     public GameObject point1;
     public GameObject point2;
     public Color segmentColor  = Color.green;
+
     private void OnDrawGizmos()
     {
-        if (point1 == null && point2 == null) return;
+        if (point1 == null || point2 == null)
+        {
+            UnityEditor.EditorApplication.delayCall+= () => DestroyImmediate(gameObject);
+        }
         
         var start = point1.transform.position;
         var end = point2.transform.position;
